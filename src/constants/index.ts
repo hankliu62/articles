90b53fdcc3d123914106f0a3ffcf948d701ccb8e
hankliu <397694072@qq.com>
2024-05-26 10:17:03 +0800

    feat(articles): add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,22 @@
+export const PageTitle = 'H.L Frontend - 前端技术文章';
+
+export const PageDescription = `欢迎来到我们的前端技术文章网站，分享代码的美学，探索前沿的技术，用文字和代码编织出属于我们的数字世界。无论你是初学者还是资深开发者，都能在这里找到共鸣，与我们一起，发现前端开发的无尽可能。`;
+
+export const PageKeywords = [
+  'frontend articles',
+  'articles',
+  'blog',
+  '前端技术博客',
+  '前端技术文章',
+  'toolkit',
+  'toolkits',
+  '前端开发',
+  '前端开发工具',
+  '前端开发工具集合',
+  '在线工具',
+  'toolbox',
+  'frontend',
+  '卡鲁秋',
+  'Hank',
+  'HankLiu',
+];
